2e80471d8d0d961d52164ae13cf3704fbd366f4e
Luke Meyers <n.luke.meyers@gmail.com>
2025-04-30 22:26:49 -0700

    Add simple expiry scheme that only checks on read.

--- cache/cache.go
+++ cache/cache.go
@@ -1,23 +1,40 @@
 package cache
 
+import "time"
+
 // A Cache is an associative in-memory store.
 type Cache struct {
-	m map[string]any
+	m map[string]entry
+}
+
+type entry struct {
+	v           any
+	expireAfter time.Time
 }
 
 // NewCache creates and returns a new, empty Cache.
 func NewCache() *Cache {
 	return &Cache{
-		m: make(map[string]any),
+		m: make(map[string]entry),
 	}
 }
 
-// Put stores value v in Cache c under key k, replacing any existing value.
-func (c *Cache) Put(k string, v any) {
-	c.m[k] = v
+// Put stores value v in Cache c under key k, replacing any existing value. The
+// value is retained until the provided TTL expires.
+func (c *Cache) Put(k string, v any, ttl time.Duration) {
+	c.m[k] = entry{v: v, expireAfter: time.Now().Add(ttl)}
 }
 
-// Get returns the value, if any, stored in Cache c under key k. If no value is stored, it returns nil.
+// Get returns the value, if any, stored in Cache c under key k. If no value is
+// stored, it returns nil.
 func (c *Cache) Get(k string) any {
-	return c.m[k]
+	ent, ok := c.m[k]
+	if !ok {
+		return nil
+	}
+	if time.Now().After(ent.expireAfter) {
+		delete(c.m, k)
+		return nil
+	}
+	return ent.v
 }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -25,10 +26,27 @@ func TestCachePutAndGet(t *testing.T) {
 
 	// When I put an integer value into the cache under that key
 	v := 42
-	c.Put(k, v)
+	c.Put(k, v, time.Hour)
 	// And I retrieve from the cache using that key
 	got := c.Get(k)
 
 	// Then I get the same value I had put in
 	assert.Equal(t, v, got)
+}
+
+func TestCacheExpireTTL(t *testing.T) {
+	// Given a value cached for some TTL
+	k := "test_key"
+	c := NewCache()
+	v := 42
+	ttl := 100 * time.Millisecond
+	c.Put(k, v, ttl)
+
+	// When I wait for a duration > the TTL
+	time.Sleep(ttl + 1)
+	// And I attempt to retrieve the cached value
+	got := c.Get(k)
+
+	// Then I get nothing back
+	assert.Nil(t, got)
 }
