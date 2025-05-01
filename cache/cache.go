ef21bedc606694d29908e09ab99728c723adb240
Luke Meyers <n.luke.meyers@gmail.com>
2025-05-01 00:14:18 -0700

    Switch to heap-based expiry scheme.
    This should be more efficient. Needs a mutex still.

--- cache/cache.go
+++ cache/cache.go
@@ -1,13 +1,15 @@
 package cache
 
 import (
+	"container/heap"
 	"sync"
 	"time"
 )
 
 // A Cache is an associative in-memory store.
 type Cache[T any] struct {
-	m sync.Map
+	m    sync.Map
+	heap heap.Interface
 }
 
 // An entry is an element stored in a Cache. It includes a record of its own
@@ -37,6 +39,7 @@ var DefaultConfig Config = Config{
 // NewCache creates and returns a new, empty Cache.
 func NewCache[T any](conf Config) *Cache[T] {
 	var c Cache[T]
+	c.heap = &expiryHeap[T]{keys: []string{}, c: &c}
 	go c.checkExpiry(conf.ExpireCheck)
 	return &c
 }
@@ -45,6 +48,7 @@ func NewCache[T any](conf Config) *Cache[T] {
 // value is retained until the provided TTL expires.
 func (c *Cache[T]) Put(k string, v T, ttl time.Duration) {
 	c.m.Store(k, entry{v: v, expireAfter: time.Now().Add(ttl)})
+	c.heap.Push(k)
 }
 
 // Get returns the value, if any, stored in Cache c under key k. If no value is
@@ -67,12 +71,41 @@ func (c *Cache[T]) Get(k string) any {
 // wasteful busy-loop.
 func (c *Cache[T]) checkExpiry(cadence time.Duration) {
 	for {
-		c.m.Range(func(k any, v any) bool {
-			if v != nil && v.(entry).expired() {
-				c.m.Delete(k)
+		for i := 0; i < c.heap.Len(); i++ {
+			k := heap.Pop(c.heap).(string)
+			v, _ := c.m.Load(k)
+			if v == nil {
+				continue
 			}
-			return true
-		})
+			ent := v.(entry)
+			if !ent.expired() {
+				heap.Push(c.heap, k)
+				break
+			}
+			c.m.Delete(k)
+		}
 		time.Sleep(cadence)
 	}
+}
+
+// An expiryHeap is a "container/heap".Heap of Cache keys, sorted by expiry (oldest pops first).
+type expiryHeap[T any] struct {
+	keys []string
+	c    *Cache[T]
+}
+
+func (h expiryHeap[T]) Len() int { return len(h.keys) }
+func (h expiryHeap[T]) Less(i, j int) bool {
+	lhs, _ := h.c.m.Load(h.keys[i])
+	rhs, _ := h.c.m.Load(h.keys[j])
+	return lhs.(entry).expireAfter.Before(rhs.(entry).expireAfter)
+}
+func (h expiryHeap[T]) Swap(i, j int) { h.keys[i], h.keys[j] = h.keys[j], h.keys[i] }
+func (h *expiryHeap[T]) Push(x any)   { h.keys = append(h.keys, x.(string)) }
+func (h *expiryHeap[T]) Pop() any {
+	old := h.keys
+	n := len(old)
+	x := old[n-1]
+	h.keys = old[0 : n-1]
+	return x
 }
